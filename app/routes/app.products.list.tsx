db38986f7a97faef2b906fa0e4cca5d5e6d0fac4
jibiki-hmr <jibiki.hmr@gmail.com>
2025-07-16 15:08:47 +0800

    ファイルパス確認

--- app/routes/app.products.list.tsx
+++ app/routes/app.products.list.tsx
@@ -1,5 +1,13 @@
 // app/routes/app.products.list.tsx
 
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { authenticate } from "../shopify.server"; // ← ✅ こちら！
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { session } = await authenticate.admin(request);
+  return null;
+}
+
 export default function ProductListPage() {
   return (
     <div style={{ padding: "2rem" }}>
